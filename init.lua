97d00e1fae0b5cf01ab899ebe477c95ee1b1cf6b
Eric Jutrzenka <eric@jutr.me>
2026-04-14 09:53:48 -0700

    Add keybind to telescope for app files including ignored

--- init.lua
+++ init.lua
@@ -454,6 +454,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files {
+          no_ignore = true,
+        }
+      end, { desc = '[S]earch All [F]iles' })
+
       vim.keymap.set('n', '<leader>si', builtin.git_files, { desc = '[S]earch G[i]t files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
