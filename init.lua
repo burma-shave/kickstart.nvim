a2d860464210609377c216f7c0ea036fc2c7fa4c
Eric Jutrzenka <eric@jutr.me>
2026-04-14 16:45:15 -0700

    Disable config on LspAttach
    Disabling for to see if I can live without it.

--- init.lua
+++ init.lua
@@ -541,11 +541,7 @@ require('lazy').setup({
       -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
       -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
+      local lspattach = {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
           -- NOTE: Remember that Lua is a real programming language, and as such it is possible
@@ -647,7 +643,12 @@ require('lazy').setup({
             end, '[T]oggle Inlay [H]ints')
           end
         end,
-      })
+      }
+      --  This function gets run when an LSP attaches to a particular buffer.
+      --    That is to say, every time a new file is opened that is associated with
+      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+      --    function will be executed to configure the current buffer
+      -- vim.api.nvim_create_autocmd('LspAttach', lspattach)
 
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
